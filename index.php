22b65da8a6ce7fce43e216a20a1c68f997d76033
Poloz Alexey <polozhev@mail.ru>
2018-04-30 22:20:04 +0300

    Change template

--- index.php
+++ index.php
@@ -1,8 +1,33 @@
 <?php
-
-$page = 'index';
 $title = 'Заказ цветов';
-
-include('sys/tpl/main.html');
-
+include('sys/main.html');
 ?>
+
+<script>
+	place('.note', 4, 85, 10, 15)
+</script>
+
+<div class="info" onclick="change(this);">↑ Нажми на лого ↑</div>
+
+<div class="note">
+	<div>
+		Товар 1
+	</div><div>
+		Товар 2
+	</div><div>
+		Товар 3
+	</div><div>	
+		Товар 4
+	</div><div>
+		Товар 5
+	</div><div>
+		Товар 6
+	</div><div>
+		Товар 7
+	</div><div>
+		Товар 8
+	</div>
+</div>
+
+</body>
+</html>
