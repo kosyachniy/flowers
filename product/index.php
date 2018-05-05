ffb07aab9c64e45266f1adf5571d91f0001068b0
Poloz Alexey <polozhev@mail.ru>
2018-05-05 16:30:26 +0300

    Design product indent

--- product/index.php
+++ product/index.php
@@ -4,14 +4,26 @@ include('../sys/head.html');
 
 $id = $_GET['i'];
 
+function nl2p($string) {
+	$paragraphs = '';
+
+	foreach (explode("\n", $string) as $line) {
+		if (trim($line)) {
+			$paragraphs .= '<p>' . $line . '</p>';
+		}
+	}
+
+	return $paragraphs;
+}
+
 $res = mysqli_query($db, "SELECT * FROM `products` WHERE `id`='$id'");
 while ($row = mysqli_fetch_array($res)) {
 	print '<div class="note">
 	<h2>' . $row['name'] . '</h2>
 	<div class="note-img"><img src="/load/products/' . $id . '.jpg"></div><div class="note-cont">';
 
-	if ($row['descr']) print '<div>' . nl2br($row['descr']) . '</div>';
-	if ($row['cont']) print '<div>' . nl2br($row['cont']) . '</div>';
+	if ($row['descr']) print '<div>' . nl2p($row['descr']) . '</div>';
+	if ($row['cont']) print '<div>' . nl2p($row['cont']) . '</div>';
 ?>
 
 </div>
