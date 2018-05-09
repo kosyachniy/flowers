fecdde64351dff1263faef1850ce2ca98381111d
Poloz Alexey <polozhev@mail.ru>
2018-05-09 12:36:59 +0300

    Main page

--- index.php
+++ index.php
@@ -4,27 +4,14 @@ include('sys/head.html');
 ?>
 
 <section id="sp-main-body-wrapper" class=" "><div class="container"><div class="row-fluid" id="main-body">
-<aside id="sp-left" class="span3">      <div class="jshop_menu_level_0">
-            <a href="http://tychinka52.ru/kategorii/category/view/2">Розы                            </a>
-      </div>
-        <div class="jshop_menu_level_0">
-            <a href="http://tychinka52.ru/kategorii/category/view/15">Тюльпаны                            </a>
-      </div>
-        <div class="jshop_menu_level_0">
-            <a href="http://tychinka52.ru/kategorii/category/view/3">Букеты цветов 990-1990₽                            </a>
-      </div>
-        <div class="jshop_menu_level_0">
-            <a href="http://tychinka52.ru/kategorii/category/view/4">Букеты цветов 2000-2990₽                            </a>
-      </div>
-        <div class="jshop_menu_level_0">
-            <a href="http://tychinka52.ru/kategorii/category/view/5">Букеты цветов от 3000₽                            </a>
-      </div>
-        <div class="jshop_menu_level_0">
-            <a href="http://tychinka52.ru/kategorii/category/view/6">Свадебные букеты невесты                            </a>
-      </div>
-        <div class="jshop_menu_level_0">
-            <a href="http://tychinka52.ru/kategorii/category/view/18">Гелиевые шары                            </a>
-      </div>
+<aside id="sp-left" class="span3"><?php
+    $res2 = mysqli_query($db, "SELECT * FROM `categories` ORDER BY `priority` DESC");
+    while ($row = mysqli_fetch_array($res2)) {
+        print '<div class="jshop_menu_level_0">
+            <a href="?i=' . $row['id'] . '">' . $row['name'] . '</a>
+      </div>';
+    }
+?>
   </aside>
 
 <div id="sp-message-area" class="span6"><section id="sp-component-area-wrapper" class=" "><div class="row-fluid" id="component-area">
@@ -51,12 +38,27 @@ include('sys/head.html');
 		
 				
 		<p>		</p><div class="moduletable">
-							<h3>Лидеры продаж:</h3>
+
+<?php
+$cat = $_GET['i'];
+if ($cat>0) {
+    $res2 = mysqli_query($db, "SELECT * FROM `categories` WHERE `id`='$cat'");
+    while ($row = mysqli_fetch_array($res2)) {
+        $cat2 = mb_strtoupper_first($row['name']);
+    }
+} else {
+    $cat2 = 'Лидеры продаж';
+}
+?>							<h3><?=$cat2?>:</h3>
 						<div class="label_products list_product">
 
 
 <?php
+if ($cat>0) {
+    $res2 = mysqli_query($db, "SELECT * FROM `products` WHERE `category`='$cat' ORDER BY `priority` DESC");
+} else {
     $res2 = mysqli_query($db, "SELECT * FROM `products` ORDER BY `priority` DESC LIMIT 9");
+}
     while ($row = mysqli_fetch_array($res2)) {
         include('sys/middle.html');
     }
@@ -88,38 +90,38 @@ include('sys/head.html');
 		</section></div></section></div>
 </div></section></div>
 
+<!--
 <aside id="sp-right" class="span3"><div class="module ">	
 	<div class="mod-wrapper clearfix">		
 					<h3 class="header">			
-				<span>Честные отзывы</span>			</h3>
+				<span>Отзывы</span>			</h3>
 								<div class="mod-content clearfix">	
 			<div class="mod-inner clearfix">
-				
-
-<div class="custom">
-	<div id="hypercomments_mix"><div class="hcc hc__mix"><div class="hcc hc__mix__title" el="Title">Последние комментарии</div><div class="hcc" el="Tabs"></div><div class="hcc hc__mix__wrap" el="Cont"><div class="hcc hc__mix__message"><div class="hcc hc__mix__avatar"><img el="Avatar" class="hcc" src="./index.php_files/64473913d30eda2c6910cbac6fb265fd" onerror="if(this.src != &#39;http://static.hypercomments.com/data/avatars/0/avatar&#39;)this.src=&#39;http://static.hypercomments.com/data/avatars/0/avatar&#39;;" title="Наталья"></div><div class="hcc hc__mix__cont"><div class="hcc hc__mix__head"><span class="hcc hc__nick">Наталья</span><span class="hcc hc__time">позавчера в 11:54</span></div><div class="hcc hc__mix__txt hc__message__txt"><div class="hcc hc__parent__txt">Спасибо огромное! У подруги день рождения, заказала букет (в описании просто сказала на Ваше усмотрение, главное, чтобы было нежно) и <span class="hcc hc__moretxt hc__hide" el="HideP">шарики с перышками. В итоге - супер красивый букет! Потрясающие шарики. Сюрприз удался)))) Подруга в приятном шоке!</span><span class="hcc hc__parent__toggle" el="More"> ещё...</span></div></div><div class="hcc hc__mix__link"><a target="_blank" href="http://tychinka52.ru/otzyvy#hcm=1525683270914595" class="hcc hc__manager__item">Оставить отзыв</a></div></div><div class="hcc hc__clear"></div></div><div class="hcc hc__mix__message"><div class="hcc hc__mix__avatar"><img el="Avatar" class="hcc" src="./index.php_files/a44e4363d57b376abe566d350762b52b" onerror="if(this.src != &#39;http://static.hypercomments.com/data/avatars/0/avatar&#39;)this.src=&#39;http://static.hypercomments.com/data/avatars/0/avatar&#39;;" title="Ольга Короткова"></div><div class="hcc hc__mix__cont"><div class="hcc hc__mix__head"><span class="hcc hc__nick">Ольга Короткова</span><span class="hcc hc__time">2017.11.20 19:08</span></div><div class="hcc hc__mix__txt hc__message__txt"><div class="hcc hc__parent__txt">Увидела на сайте букет со скидкой из 51 розы, попросила добавить туда ещё 4 розы, сотрудники собрали букет также со <span class="hcc hc__moretxt hc__hide" el="HideP">скидкой, заказала доставку на 9 утра, переливала, что опоздает доставка, а нет, приехали даже на 15 минут раньше. Букет свежий, большое количество роз аккуратно сформированы в букет! Всем советую, обязательно закажу ещё! Спасибо за качественный товар и услугу!</span><span class="hcc hc__parent__toggle" el="More"> ещё...</span></div></div><div class="hcc hc__mix__link"><a target="_blank" href="http://tychinka52.ru/otzyvy#hcm=1511194130578587" class="hcc hc__manager__item">Оставить отзыв</a></div></div><div class="hcc hc__clear"></div></div><div class="hcc hc__mix__message"><div class="hcc hc__mix__avatar"><img el="Avatar" data-href="http://profile.hypercomments.com/9236717" class="hcc" src="./index.php_files/avatar" onerror="if(this.src != &#39;http://static.hypercomments.com/data/avatars/0/avatar&#39;)this.src=&#39;http://static.hypercomments.com/data/avatars/0/avatar&#39;;" title="Марина Борисова"></div><div class="hcc hc__mix__cont"><div class="hcc hc__mix__head"><span class="hcc hc__nick">Марина Борисова</span><span class="hcc hc__time">2017.11.14 23:15</span></div><div class="hcc hc__mix__txt hc__message__txt"><div class="hcc hc__parent__txt">Спасибо Ирине за помощь в составлении и оформлении букетов на день воспитателя.<br>Нам надо было уложиться в определенную и к тому <span class="hcc hc__moretxt hc__hide" el="HideP">же не большую сумму.<br>Все остались очень довольны</span><span class="hcc hc__parent__toggle" el="More"> ещё...</span></div></div><div class="hcc"><div class="hcc hc__message__attimg"><img class="hcc" alt="" el="WatchImg" src="./index.php_files/1510690498864352"></div><br><div class="hcc hc__message__attimg"><img class="hcc" alt="" el="WatchImg" src="./index.php_files/1510690516364477"></div></div><div class="hcc hc__mix__link"><a target="_blank" href="http://tychinka52.ru/otzyvy#hcm=1510690533535187" class="hcc hc__manager__item">Оставить отзыв</a></div></div><div class="hcc hc__clear"></div></div><div class="hcc hc__mix__message"><div class="hcc hc__mix__avatar"><img el="Avatar" class="hcc" src="./index.php_files/fef079855dbe2397f6837b968d355fad" onerror="if(this.src != &#39;http://static.hypercomments.com/data/avatars/0/avatar&#39;)this.src=&#39;http://static.hypercomments.com/data/avatars/0/avatar&#39;;" title="Алексей"></div><div class="hcc hc__mix__cont"><div class="hcc hc__mix__head"><span class="hcc hc__nick">Алексей</span><span class="hcc hc__time">2017.10.14 15:00</span></div><div class="hcc hc__mix__txt hc__message__txt"><div class="hcc hc__parent__txt">Спасибо огромное. Очень хороший букет и шарики по очень разумной цене. Сразу понятно - команда профессионалов. Хочу пожелать Вам немного, <span class="hcc hc__moretxt hc__hide" el="HideP">потому что всё и так на высоте - процветания. И ещё одна просьба - продумайте чтобы курьер привозил ещё хотя бы визитку или буклет, чтобы можно было советовать Ваш магазин друзьям, родственникам и знакомым.</span><span class="hcc hc__parent__toggle" el="More"> ещё...</span></div></div><div class="hcc hc__mix__link"><a target="_blank" href="http://tychinka52.ru/otzyvy#hcm=1507982406857103" class="hcc hc__manager__item">Оставить отзыв</a></div></div><div class="hcc hc__clear"></div></div><div class="hcc hc__mix__message"><div class="hcc hc__mix__avatar"><img el="Avatar" data-href="http://profile.hypercomments.com/9097292" class="hcc" src="./index.php_files/avatar(1)" onerror="if(this.src != &#39;http://static.hypercomments.com/data/avatars/0/avatar&#39;)this.src=&#39;http://static.hypercomments.com/data/avatars/0/avatar&#39;;" title="Никита Егоров"></div><div class="hcc hc__mix__cont"><div class="hcc hc__mix__head"><span class="hcc hc__nick">Никита Егоров</span><span class="hcc hc__time">2017.10.10 15:13</span></div><div class="hcc hc__mix__txt hc__message__txt">и я доволен</div><div class="hcc hc__mix__link"><a target="_blank" href="http://tychinka52.ru/otzyvy#hcm=1507637598407140" class="hcc hc__manager__item">Оставить отзыв</a></div></div><div class="hcc hc__clear"></div></div></div></div></div>
-<script type="text/javascript">
-_hcwp = window._hcwp || [];
-_hcwp.push({widget:"Mixstream", widget_id:18122, filter:"last", limit:5});
-(function() {
-if("HC_LOAD_INIT" in window)return;
-HC_LOAD_INIT = true;
-var lang = (navigator.language || navigator.systemLanguage || navigator.userLanguage ||  "en").substr(0, 2).toLowerCase();
-var hcc = document.createElement("script"); hcc.type = "text/javascript"; hcc.async = true;
-hcc.src = ("https:" == document.location.protocol ? "https" : "http")+"://w.hypercomments.com/widget/hc/18122/"+lang+"/widget.js";
-var s = document.getElementsByTagName("script")[0];
-s.parentNode.insertBefore(hcc, s.nextSibling);
-})();
-</script></div>
+	
 			</div>
 		</div>
 	</div>
 </div>
 <div class="gap"></div>
 </aside>
+!-->
 </div></div></section>
 
 
+
+<style>
+@media all and (min-width: 768px) {
+    #sp-message-area {width: 70%;}
+}
+</style>
+
+
+<div style="width: 80%; margin: 0 0 50px 10%;">
+    <div id="vk_comments"></div>
+    <script type="text/javascript">
+    VK.Widgets.Comments("vk_comments", {limit: 5, attach: "*"});
+    </script>
+</div>
 
 <div style="width: 100%; height: 500px;">
     <a class="dg-widget-link" href="http://2gis.ru/n_novgorod/firm/2674541559940084/center/43.95288705825806,56.340401698160264/zoom/16?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=bigMap">Посмотреть на карте Нижнего Новгорода</a><div class="dg-widget-link"><a href="http://2gis.ru/n_novgorod/firm/2674541559940084/photos/2674541559940084/center/43.95288705825806,56.340401698160264/zoom/17?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=photos">Фотографии компании</a></div><div class="dg-widget-link"><a href="http://2gis.ru/n_novgorod/center/43.95289,56.339959/zoom/16/routeTab/rsType/bus/to/43.95289,56.339959╎Цветочка, букетная лавка?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=route">Найти проезд до Цветочка, букетная лавка</a></div><script charset="utf-8" src="https://widgets.2gis.com/js/DGWidgetLoader.js"></script><script charset="utf-8">new DGWidgetLoader({"width":640,"height":600,"borderColor":"#a3a3a3","pos":{"lat":56.340401698160264,"lon":43.95288705825806,"zoom":16},"opt":{"city":"n_novgorod"},"org":[{"id":"2674541559940084"}]});</script><noscript style="color:#c00;font-size:16px;font-weight:bold;">Виджет карты использует JavaScript. Включите его в настройках вашего браузера.</noscript>
