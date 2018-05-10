9f4f9cbeef4fbc2fa7ebfe1ca658ac6e40cd53b2
Poloz Alexey <polozhev@mail.ru>
2018-05-10 23:19:14 +0300

    DB

--- sys/func.php
+++ sys/func.php
@@ -3,7 +3,7 @@ header('Content-type: text/html; charset=utf-8');
 session_start();
 
 function db($name='main') {
-	$db = mysqli_connect('mysql.hostinger.ru', 'u489860185_main', 'asdrqwerty09', 'u489860185_'.$name);
+	$db = mysqli_connect('mysql.hostinger.ru', 'u869945297_main', 'asdrqwerty09', 'u869945297_'.$name);
 	if (mysqli_connect_errno()) print 'Ошибка 1. '.mysqli_connect_errno();
 	mysqli_query($db, 'SET names "utf8"');
 	return $db;
